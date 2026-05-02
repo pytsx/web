24a3cedd7a0827b3fb0748e1b41643db75ff2d0b
Henrique Pessoa <130256224+pytsx@users.noreply.github.com>
2026-05-02 15:56:28 +0000

    Remove parâmetro `group` dos métodos e referencia `g.prefix`

--- group.go
+++ group.go
@@ -12,24 +12,24 @@ func NewGroup(app *App, prefix string) *Group {
 	}
 }
 
-func (g *Group) Get(group string, path string, handlerFunc HandlerFunc, mw ...MidFunc) {
-	g.app.Get(group, path, handlerFunc, mw...)
+func (g *Group) Get(path string, handlerFunc HandlerFunc, mw ...MidFunc) {
+	g.app.Get(g.prefix, path, handlerFunc, mw...)
 }
 
-func (g *Group) Post(group string, path string, handlerFunc HandlerFunc, mw ...MidFunc) {
-	g.app.Post(group, path, handlerFunc, mw...)
+func (g *Group) Post(path string, handlerFunc HandlerFunc, mw ...MidFunc) {
+	g.app.Post(g.prefix, path, handlerFunc, mw...)
 }
 
-func (g *Group) Put(group string, path string, handlerFunc HandlerFunc, mw ...MidFunc) {
-	g.app.Put(group, path, handlerFunc, mw...)
+func (g *Group) Put(path string, handlerFunc HandlerFunc, mw ...MidFunc) {
+	g.app.Put(g.prefix, path, handlerFunc, mw...)
 }
 
-func (g *Group) Patch(group string, path string, handlerFunc HandlerFunc, mw ...MidFunc) {
-	g.app.Patch(group, path, handlerFunc, mw...)
+func (g *Group) Patch(path string, handlerFunc HandlerFunc, mw ...MidFunc) {
+	g.app.Patch(g.prefix, path, handlerFunc, mw...)
 }
 
-func (g *Group) Delete(group string, path string, handlerFunc HandlerFunc, mw ...MidFunc) {
-	g.app.Delete(group, path, handlerFunc, mw...)
+func (g *Group) Delete(path string, handlerFunc HandlerFunc, mw ...MidFunc) {
+	g.app.Delete(g.prefix, path, handlerFunc, mw...)
 }
 
 func (g *Group) HandleFunc(method string, path string, handlerFunc HandlerFunc, mw ...MidFunc) {
